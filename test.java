91f9aa254ce15fe1055a2bc35e98882d151ab4a4
darshank26 <darsh2605@gmail.com>
2017-08-05 15:54:25 +0530

    Update test.java

--- test.java
+++ test.java
@@ -11,157 +11,157 @@ class test
 		  for(int i=0;i<name.length();i++)
 		  {	  char a = name.charAt(i);
 	          System.out.println("\n");
-	          if ( a == ('A' | 'a' ) )
+	          if (( a =='A') || (a=='a' ) )
 			  {
 				  System.out.println(" ####\n##  ##\n######\n##  ##\n##  ##");
 				  	          System.out.println("\n");
 
 			  }
-			  if ( a == ('B' | 'b' ) )
+			  if (( a =='B') || (a=='b' ) )
 			  {
 				  System.out.println("####\n##  #\n####\n##  #\n####");
 				  	          System.out.println("\n");
 
 			  }
-			   if ( a == ('C' | 'c' ) )
+			   if (( a =='C') || (a=='c' ) )
 			  {
 				  System.out.println("  ####\n ##  \n## \n ##  \n  ####");
 				  	          System.out.println("\n");
 
 				  
 			  }
-			   if ( a == ('D' | 'd' ) )
+			   if (( a =='D') || (a=='d' ) )
 			  {
 				  System.out.println("####\n##  ##\n##  ##\n##  ##\n####");
 				  	          System.out.println("\n");
 
 			  }
-			   if ( a == ('E' | 'e' ) )
+			   if(( a =='E') || (a=='e' ) )
 			  {
 				  System.out.println("#####\n##\n#####\n##\n#####");
 				  	          System.out.println("\n");
 
 			  }
-			  if ( a == ('F' | 'f' ) )
+			  if(( a =='F') || (a=='f' ) )
 			  {
 				  System.out.println("#####\n##\n#####\n##\n##");
 				  	          System.out.println("\n");
 
 			  }
-			  if ( a == ('G' | 'g' ) )
+			  if (( a =='G') || (a=='g' ) )
 			  {
 				  System.out.println(" ##### \n##   \n##  ### \n ### ##");
 				  	          System.out.println("\n");
 
 			  }
-			  if ( a == ('H' | 'h' ) )
+			  if(( a =='H') || (a=='h' ) )
 			  {
 				  System.out.println("##   ##\n##   ##\n#######\n##   ##\n##   ##");
 				  	          System.out.println("\n");
 
 			  }
-			  if ( a == ('I' | 'i' ) )
+			  if(( a =='I') || (a=='i' ) )
 			  {
 				  System.out.println("######  \n  ##  \n  ##  \n  ##\n######");
 				  	          System.out.println("\n");
 
 			  }
-			  if ( a == ('J' | 'j' ) )
+			  if (( a =='J') || (a=='j' ) )
 			  {
 				  System.out.println("#######\n  ##  \n  ##\n# ## \n ##");
 				  	          System.out.println("\n");
 
 			  }
-			  if ( a == ('K' | 'k' ) )
+			  if (( a =='K') || (a=='k' ) )
 			  {
 				  System.out.println("##  ##\n## ##\n###\n## ##\n##  ##");
 				  	          System.out.println("\n");
 
 			  }
-			  if ( a == ('L' | 'l' ) )
+			  if(( a =='L') || (a=='l' ) )
 			  {
 				  System.out.println("##\n##\n##\n##\n######");
 				  	          System.out.println("\n");
 
 			  }
-			  if ( a == ('M' | 'm' ) )
+			  if (( a =='M') || (a=='m' ) )
 			  {
 				  System.out.println("##    ##\n# #  # #\n#  ##  #\n#      #\n#      #");
 				  	          System.out.println("\n");
 
 			  }
-			  if ( a == ('N' | 'n' ) )
+			  if (( a =='N') || (a=='n' ) )
 			  {
 				  System.out.println("##   #\n# #  #\n#  # #\n#   ##\n#    #");
 				  	          System.out.println("\n");
 
 			  }
-			  if ( a == ('O' | 'o' ) )
+			  if (( a =='O') || (a=='o' ) )
 			  {
 				  System.out.println("  ##\n##  ##\n##  ##\n##  ##\n  ##");
 				  	          System.out.println("\n");
 
 			  }
-			  if ( a == ('P' | 'p' ) )
+			  if(( a =='P') || (a=='p' ) )
 			  {
 				  System.out.println("####\n##  #\n####\n##\n##");
 				  	          System.out.println("\n");
 
-			  }if ( a == ('Q' | 'q' ) )
+			  }if (( a =='Q') || (a=='q' ) )
 			  {
 				  System.out.println("  ###\n##   ##\n## # ##\n##  ##\n  ## ##");
 				  	          System.out.println("\n");
 
 			  }
-			  if ( a == ('R' | 'r' ) )
+			  if (( a =='R') || (a=='r' ) )
 			  {
 				  System.out.println("####\n##  #\n####\n##  #\n##  #");
 				  	          System.out.println("\n");
 
 			  }
-			  if ( a == ('S' | 's' ) )
+			  if (( a =='S') || (a=='s' ) )
 			  {
 				  System.out.println(" ####\n##\n  ##\n   ##\n####");
 				  	          System.out.println("\n");
 
 			  }
-			  if ( a == ('T' | 't' ) )
+			  if (( a =='T') || (a=='t' ) )
 			  {
 				  System.out.println("######\n  ##\n  ##\n  ##");
 				  	          System.out.println("\n");
 
 			  }
-			  if ( a == ('U' | 'u' ) )
+			  if(( a =='U') || (a=='u' ) )
 			  {
 				  System.out.println("##  ##\n##  ##\n##  ##\n##  ##\n######");
 				  	          System.out.println("\n");
 
 			  }
-			  if ( a == ('V' | 'v' ) )
+			  if (( a =='V') || (a=='v' ) )
 			  {
 				  System.out.println("##  ##\n##  ##\n##  ##\n##  ##\n  ##");
 				  	          System.out.println("\n");
 
 			  }
-			  if ( a == ('W' | 'w' ) )
+			  if(( a =='W') || (a=='w' ) )
 			  {
 				  System.out.println("##       ##\n##   #   ##\n##  # #  ##\n## #   # ##\n  #     # ");
 				  	          System.out.println("\n");
 
 			  }
-			  if ( a == ('X' | 'x' ) )
+			  if(( a =='X') || (a=='x' ) )
 			  {
-				  System.out.println("##   ##\n  ## ##\n   #\n ## ##\n##   ##");
+				  System.out.println("##   ##\n ## ##\n   #\n ## ##\n##   ##");
 				  	          System.out.println("\n");
 
 			  }
-			  if ( a == ('Y' | 'y' ) )
+			  if(( a =='Y') || (a=='y' ) )
 			  {
 				  System.out.println("##   ##\n ## ##\n  ##\n  ##\n  ##");
 				  	          System.out.println("\n");
 
 			  }
-			  if ( a == ('Z' | 'z' ) )
+			  if(( a =='Z') || (a=='z' ) )
 			  {
 				  System.out.println("######\n   ##\n  ##\n ##\n######");
 				  	          System.out.println("\n");
@@ -172,9 +172,7 @@ class test
 				  System.out.println("\n");
 				  	          System.out.println("\n");
 
-			  }
-			  
-			  
+			  }  
 		  }
 	 }
 }
