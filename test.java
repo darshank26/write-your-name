837b48f1691c47ec5ea496ce27b35069110c2f9a
darshank26 <darsh2605@gmail.com>
2017-08-05 15:13:35 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,180 @@
+import java.util.*;
+
+class test 
+{
+	 public static void main(String args[])
+	 {
+		  Scanner sc = new Scanner(System.in);
+		  System.out.println("Enter your name: ");
+		  String name = new String();
+		  name = sc.nextLine();
+		  for(int i=0;i<name.length();i++)
+		  {	  char a = name.charAt(i);
+	          System.out.println("\n");
+	          if ( a == ('A' | 'a' ) )
+			  {
+				  System.out.println(" ####\n##  ##\n######\n##  ##\n##  ##");
+				  	          System.out.println("\n");
+
+			  }
+			  if ( a == ('B' | 'b' ) )
+			  {
+				  System.out.println("####\n##  #\n####\n##  #\n####");
+				  	          System.out.println("\n");
+
+			  }
+			   if ( a == ('C' | 'c' ) )
+			  {
+				  System.out.println("  ####\n ##  \n## \n ##  \n  ####");
+				  	          System.out.println("\n");
+
+				  
+			  }
+			   if ( a == ('D' | 'd' ) )
+			  {
+				  System.out.println("####\n##  ##\n##  ##\n##  ##\n####");
+				  	          System.out.println("\n");
+
+			  }
+			   if ( a == ('E' | 'e' ) )
+			  {
+				  System.out.println("#####\n##\n#####\n##\n#####");
+				  	          System.out.println("\n");
+
+			  }
+			  if ( a == ('F' | 'f' ) )
+			  {
+				  System.out.println("#####\n##\n#####\n##\n##");
+				  	          System.out.println("\n");
+
+			  }
+			  if ( a == ('G' | 'g' ) )
+			  {
+				  System.out.println(" ##### \n##   \n##  ### \n ### ##");
+				  	          System.out.println("\n");
+
+			  }
+			  if ( a == ('H' | 'h' ) )
+			  {
+				  System.out.println("##   ##\n##   ##\n#######\n##   ##\n##   ##");
+				  	          System.out.println("\n");
+
+			  }
+			  if ( a == ('I' | 'i' ) )
+			  {
+				  System.out.println("######  \n  ##  \n  ##  \n  ##\n######");
+				  	          System.out.println("\n");
+
+			  }
+			  if ( a == ('J' | 'j' ) )
+			  {
+				  System.out.println("#######\n  ##  \n  ##\n# ## \n ##");
+				  	          System.out.println("\n");
+
+			  }
+			  if ( a == ('K' | 'k' ) )
+			  {
+				  System.out.println("##  ##\n## ##\n###\n## ##\n##  ##");
+				  	          System.out.println("\n");
+
+			  }
+			  if ( a == ('L' | 'l' ) )
+			  {
+				  System.out.println("##\n##\n##\n##\n######");
+				  	          System.out.println("\n");
+
+			  }
+			  if ( a == ('M' | 'm' ) )
+			  {
+				  System.out.println("##    ##\n# #  # #\n#  ##  #\n#      #\n#      #");
+				  	          System.out.println("\n");
+
+			  }
+			  if ( a == ('N' | 'n' ) )
+			  {
+				  System.out.println("##   #\n# #  #\n#  # #\n#   ##\n#    #");
+				  	          System.out.println("\n");
+
+			  }
+			  if ( a == ('O' | 'o' ) )
+			  {
+				  System.out.println("  ##\n##  ##\n##  ##\n##  ##\n  ##");
+				  	          System.out.println("\n");
+
+			  }
+			  if ( a == ('P' | 'p' ) )
+			  {
+				  System.out.println("####\n##  #\n####\n##\n##");
+				  	          System.out.println("\n");
+
+			  }if ( a == ('Q' | 'q' ) )
+			  {
+				  System.out.println("  ###\n##   ##\n## # ##\n##  ##\n  ## ##");
+				  	          System.out.println("\n");
+
+			  }
+			  if ( a == ('R' | 'r' ) )
+			  {
+				  System.out.println("####\n##  #\n####\n##  #\n##  #");
+				  	          System.out.println("\n");
+
+			  }
+			  if ( a == ('S' | 's' ) )
+			  {
+				  System.out.println(" ####\n##\n  ##\n   ##\n####");
+				  	          System.out.println("\n");
+
+			  }
+			  if ( a == ('T' | 't' ) )
+			  {
+				  System.out.println("######\n  ##\n  ##\n  ##");
+				  	          System.out.println("\n");
+
+			  }
+			  if ( a == ('U' | 'u' ) )
+			  {
+				  System.out.println("##  ##\n##  ##\n##  ##\n##  ##\n######");
+				  	          System.out.println("\n");
+
+			  }
+			  if ( a == ('V' | 'v' ) )
+			  {
+				  System.out.println("##  ##\n##  ##\n##  ##\n##  ##\n  ##");
+				  	          System.out.println("\n");
+
+			  }
+			  if ( a == ('W' | 'w' ) )
+			  {
+				  System.out.println("##       ##\n##   #   ##\n##  # #  ##\n## #   # ##\n  #     # ");
+				  	          System.out.println("\n");
+
+			  }
+			  if ( a == ('X' | 'x' ) )
+			  {
+				  System.out.println("##   ##\n  ## ##\n   #\n ## ##\n##   ##");
+				  	          System.out.println("\n");
+
+			  }
+			  if ( a == ('Y' | 'y' ) )
+			  {
+				  System.out.println("##   ##\n ## ##\n  ##\n  ##\n  ##");
+				  	          System.out.println("\n");
+
+			  }
+			  if ( a == ('Z' | 'z' ) )
+			  {
+				  System.out.println("######\n   ##\n  ##\n ##\n######");
+				  	          System.out.println("\n");
+
+			  }
+			  if ( a == (' ' ) )
+			  {
+				  System.out.println("\n");
+				  	          System.out.println("\n");
+
+			  }
+			  
+			  
+		  }
+	 }
+}
